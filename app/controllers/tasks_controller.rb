c19000de4e51cf48d652c1e8064d56aafc8cd470
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-05 01:03:30 +0900

    new・create・edit・updateアクション作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,22 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
+  before_action :set_url, only: %i[new create edit update]
 
   def index
     @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.build(task_params)
+    if @task.save
+      redirect_to @url, notice: "登録完了しました"
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,6 +26,11 @@ class TasksController < ApplicationController
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to @url, notice: "更新完了しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -30,5 +43,9 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def set_url
+    @url = request.referer
   end
 end
