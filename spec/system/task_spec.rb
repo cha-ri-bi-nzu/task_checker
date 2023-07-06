4063f1f16b6416fe447680629ba4d151869dd248
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-06 21:45:40 +0900

    新規作成機能テスト成功

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  # describe '新規作成機能' do
-  #   context 'タスクを新規作成した場合' do
-  #     it '作成したタスクが表示される' do
-  #     end
-  #   end
-  # end
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in "task[name]", with: 'task_name'
+        fill_in "task[content]", with: 'content_1'
+        click_button "登録"
+        visit task_path(Task.last)
+        expect('task_name').to have_content 'name'
+        expect('content_1').to have_content 'content'
+      end
+    end
+  end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         FactoryBot.create(:task, name: 'task')
         visit tasks_path
-        expect(page).to have_content 'task_failure'
+        expect(page).to have_content 'task'
       end
     end
   end
