9b8a366ac747af39bce6f31738eb6686fdc26347
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-10 01:12:17 +0900

    statusカラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_09_094725) do
+ActiveRecord::Schema.define(version: 2023_07_09_153608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_07_09_094725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "time_limit", null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
