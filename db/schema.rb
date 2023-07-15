7a5d8698f683a2554eeb6303b696ae0dd00b751c
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-15 16:35:15 +0900

    tasksテーブルのname, statusにindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_09_153608) do
+ActiveRecord::Schema.define(version: 2023_07_15_072916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2023_07_09_153608) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "time_limit", null: false
     t.integer "status", default: 0, null: false
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
