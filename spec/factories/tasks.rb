b36fbc9212e77029a2b361a6ad4c30c6a609dd9b
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-18 14:31:57 +0900

    rspecテスト準備

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,15 +3,18 @@ FactoryBot.define do
     name {'test_name'}
     content {'test_content'}
     status {'未着手'}
+    priority {'低'}
   end
   factory :second_task, class: Task do
     name {'Factoryで作ったデフォルトのタスク2'}
     content {'Factoryで作ったデフォルトのコンテント2'}
     status {'着手中'}
+    priority {'高'}
   end
   factory :third_task, class: Task do
     name {'Factoryで作ったデフォルトのタスク3'}
     content {'Factoryで作ったデフォルトのコンテント3'}
     status {'完了'}
+    priority {'中'}
   end
 end
