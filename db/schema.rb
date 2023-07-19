ed20d13d180a4185421e1125c70afdf2909cf251
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-20 00:23:45 +0900

    Userテーブルにadminカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_19_025727) do
+ActiveRecord::Schema.define(version: 2023_07_19_151936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2023_07_19_025727) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["name"], name: "index_users_on_name"
   end
 
