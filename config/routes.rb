878e6590a2b0db066b2d766358df94314a73c695
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-19 16:12:33 +0900

    admin/usersのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/index'
-    get 'users/new'
-    get 'users/create'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/destroy'
-  end
   root "tasks#index"
 
   resources :tasks
   resources :sessions, only: %i[new create destroy]
-
+  namespace :admin do
+    resources path :users
+  end
 end
