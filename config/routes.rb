a92672f7015cd8486b3585b899d6018cbc94c611
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-19 17:50:21 +0900

    ルーティングに一般ユーザー用のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :tasks
   resources :sessions, only: %i[new create destroy]
+  resources :users, only: %i[new create show edit update destroy]
   namespace :admin do
     resources :users
   end
