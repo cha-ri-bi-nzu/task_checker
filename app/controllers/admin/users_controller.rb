6cac89566e25f91c729163aed80ce5429925cc63
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-19 17:43:49 +0900

    new・createアクション仮実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,9 +3,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find(params_user)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +25,10 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :task_attributes: %i[id name time_limit status priority created_at destroy])
   end
 end
