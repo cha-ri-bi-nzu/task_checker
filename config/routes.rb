d3e02b2b745087e5ac69e14c0a871b899d0315c1
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-19 16:04:19 +0900

    Userコントローラ・view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'uses/index'
+    get 'uses/new'
+    get 'uses/create'
+    get 'uses/show'
+    get 'uses/edit'
+    get 'uses/update'
+    get 'uses/destroy'
+  end
   root "tasks#index"
   
   resources :tasks
