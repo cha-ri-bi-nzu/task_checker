b13021bddcce58b10c5f8336448e1252b7664847
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-20 16:02:24 +0900

    admin/userのルーティング修正(show→index)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[new create show edit update destroy]
   namespace :admin do
-    resources :users
+    resources :users, only: %i[index new create edit update destroy]
   end
 end
