8274577118e12be90accccd6d308170dfffe412f
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-20 15:33:20 +0900

    ヘルパーモジュール作成、task作成・編集で遷移先のurlを変更

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,9 @@
 module TasksHelper
+  def choose_action_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      tasks_params
+    else action_name == 'edit' || action_name == 'update'
+      task_params
+    end
+  end
 end
