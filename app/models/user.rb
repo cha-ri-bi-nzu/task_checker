5edda00c188b314d2fe50599136f79b06c21266c
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-21 11:37:08 +0900

    バリデーション追加、それに伴いデータベース内一掃

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :tasks
   accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: :all_blank
 
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  before_validation {email.downcase!}
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: {minimum: 6}
 end
