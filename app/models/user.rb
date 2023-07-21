95516316cab2acd3b4cdfb064018bba5bd310c0a
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-21 16:22:10 +0900

    userと一緒にユーザーのタスクも消される紐付けの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
-  accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: :all_blank
+  has_many :tasks, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
