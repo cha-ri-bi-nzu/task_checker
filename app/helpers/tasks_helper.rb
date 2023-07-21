f0a4635856261bf40a67d2d0ca97d5ac8f1a12b7
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-21 10:49:44 +0900

    パーシャルフォームのurl用のヘルパーモジュール修正・作成

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,9 @@
 module TasksHelper
   def choose_action_new_or_edit
     if action_name == 'new' || action_name == 'create'
-      tasks_params
+      tasks_path
     else action_name == 'edit' || action_name == 'update'
-      task_params
+      task_path
     end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def choose_action_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      users_path
+    else action_name == 'edit' || action_name == 'update'
+      user_path
+    end
+  end
 end
