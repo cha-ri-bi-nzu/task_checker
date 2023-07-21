bff229ef45c9a29ebaa8e8bae5a50c668d80b08e
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-22 00:53:05 +0900

    current_userとadminのみedit・showアクションが使える制約

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
-  before_action :are_you_current_user?, only: %i[show edit update]
+  before_action :current_user_or_admin, only: %i[show edit update]
 
   def new
     @user = User.new
@@ -25,7 +25,11 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(users_params)
-      redirect_to user_path, notice: "プロフィール編集完了"
+      if current_user.admin?
+        redirect_to admin_user_path, notice: "#{this_user}さんのプロフィールを編集しました"
+      else
+        redirect_to user_path, notice: "プロフィール編集完了"
+      end
     else
       render :edit
     end
@@ -39,14 +43,14 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :admin, task_attributes: %i[id name time_limit status priority created_at _destroy])
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :admin)
   end
 
   def set_user
     @user = User.find(params[:id])
   end
 
-  def are_you_current_user?
-    redirect_to tasks_path(@user.id) unless @user.id == current_user.id
+  def current_user_or_admin
+    redirect_to tasks_path(@user.id) unless @user == current_user || current_user.admin?
   end
 end
