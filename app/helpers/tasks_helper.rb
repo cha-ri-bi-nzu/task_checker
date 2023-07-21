e7897018a10a067b451dc2d63ac61d5a5696f243
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-21 16:16:43 +0900

    新規登録が保存されないエラーを解消(urlの選択を削除)

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,2 @@
 module TasksHelper
-  def choose_action_new_or_edit
-    if action_name == 'new' || action_name == 'create'
-      tasks_path
-    else action_name == 'edit' || action_name == 'update'
-      task_path
-    end
-  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-  def choose_action_new_or_edit
-    if action_name == 'new' || action_name == 'create'
-      users_path
-    else action_name == 'edit' || action_name == 'update'
-      user_path
-    end
-  end
 end
