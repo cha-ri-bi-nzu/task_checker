641202e22cb531942017ddf7adfa154360b876c1
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-23 01:13:01 +0900

    spec/factories/users.rbにユーザーデータ登録

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,26 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
+    name {'user_name1'}
+    email {'1ban@mail.com'}
+    password {'aaaaaa'}
+    admin {'管理者'}
+  end
+  factory :second_user, class: User do
+    name {'user_name2'}
+    email {'2ban@mail.com'}
+    password {'bbbbbb'}
+    admin {'一般'}
+  end
+  factory :third_user, class: User do
+    name {'iltsupan_user'}
+    email {'iltsupan@mail.com'}
+    password {'cccccc'}
+    admin {'一般'}
+  end
+  factory :fourth_user, class: User do
+    name {'admin_user'}
+    email {'admin@mail.com'}
+    password {'dddddd'}
+    admin {'管理者'}
   end
 end
