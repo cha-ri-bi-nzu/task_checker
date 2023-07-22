0d86aec394c0a172030314dd58a68e0c9158973e
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-22 22:39:54 +0900

    ログインしていないユーザーにログインを促すメッセージ表示機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   private
   def login_required
-    redirect_to new_session_path unless current_user
+    redirect_to new_session_path, notice: "ログインをして下さい" unless current_user
   end
 end
