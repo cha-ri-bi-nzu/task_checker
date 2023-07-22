8ba880931e86ed692210250ca055cb7641e04643
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-22 22:34:35 +0900

    %1を%iに修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :login_required, only: %1[new create]
+  skip_before_action :login_required, only: %i[new create]
   before_action :set_user, only: %i[show edit update destroy]
   before_action :current_user_or_admin, only: %i[show edit update]
 
