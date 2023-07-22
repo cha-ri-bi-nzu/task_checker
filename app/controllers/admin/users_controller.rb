37c1060fbe0e513508f629b0bfeaf51afb79ef6c
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-22 14:43:16 +0900

    一部変数修正、不要なアクション・メソッドを削除

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,18 @@
 class Admin::UsersController < ApplicationController
   before_action :are_you_admin?
-  before_action :set_user, only: %i[edit update destroy]
-  before_action :this_user, only: %i[update destroy]
 
   def index
     @users = User.select(:id, :name).includes(:tasks).page(params[:page]).per(10)
   end
   
   def destroy
+    @user = User.find(params[:id])
+    lost_user = @user.name
     @user.destroy
-    redirect_to admin_users_path, notice: "#{this_user}さんのアカウントを削除しました"
+    redirect_to admin_users_path, notice: "#{lost_user}さんのアカウントを削除しました"
   end
 
   private
-  def users_params
-    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :admin)
-  end
-
   def are_you_admin?
     redirect_to tasks_path(@user.id) unless current_user.admin?
   end
