88904d3f53fcd159ebdb32e7babfd759a26d3772
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-23 14:09:05 +0900

    modelspecにユーザーデータを追記

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -16,16 +16,18 @@ RSpec.describe Task, type: :model do
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(name: 'テスト', content: '成功')
+        user = User.new(name: 'テストさん')
+        task = Task.new(name: 'テスト', content: '成功', user: user)
         expect(task).to be_valid
       end
     end
   end
   describe '検索機能' do
     # 必要に応じて、テストデータの内容を変更して構わない
-    let!(:task) { FactoryBot.create(:task, name: 'task', status: "未着手") }
-    let!(:second_task) { FactoryBot.create(:second_task, name: "sample", status: "未着手") }
-    let!(:third_task) { FactoryBot.create(:third_task, name: "task01", status: "着手中") }
+    let!(:user) { FactoryBot.create(:user, name: 'user_name1') }
+    let!(:task) { FactoryBot.create(:task, name: 'task', status: "未着手", user: user) }
+    let!(:second_task) { FactoryBot.create(:second_task, name: "sample", status: "未着手", user: user) }
+    let!(:third_task) { FactoryBot.create(:third_task, name: "task01", status: "着手中", user: user) }
     context 'scopeメソッドでタイトルのあいまい検索をした場合' do
       it "検索キーワードを含むタスクが絞り込まれる" do
         # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
