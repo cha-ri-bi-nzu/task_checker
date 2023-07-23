1a471d6a78c8fcbce63ddf747cc6a53282185392
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-23 14:08:36 +0900

    ユーザーデータを紐付け

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,17 +4,20 @@ FactoryBot.define do
     content {'test_content'}
     status {'未着手'}
     priority {'低'}
+    user {'user_name1'}
   end
   factory :second_task, class: Task do
     name {'Factoryで作ったデフォルトのタスク2'}
     content {'Factoryで作ったデフォルトのコンテント2'}
     status {'着手中'}
     priority {'高'}
+    user {'user_name1'}
   end
   factory :third_task, class: Task do
     name {'Factoryで作ったデフォルトのタスク3'}
     content {'Factoryで作ったデフォルトのコンテント3'}
     status {'完了'}
     priority {'中'}
+    user {'user_name1'}
   end
 end
