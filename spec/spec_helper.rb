23f9993bda583cab1f6416a8799546746c00c53c
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-23 15:30:38 +0900

    記入漏れ追記

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'database_cleaner' # 先頭に追記
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
