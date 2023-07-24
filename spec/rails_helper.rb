3622c904a76db9053aca38ff1570e84617ba5dba
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-24 19:09:48 +0900

    インデントの修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,13 +67,13 @@ RSpec.configure do |config|
   #   end
   # end
   config.include FactoryBot::Syntax::Methods
-    config.before(:each) do |example|
-      if example.metadata[:type] == :system
-        if example.metadata[:js]
-          driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
-        else
-          driven_by :rack_test
-        end
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
       end
     end
+  end
 end
